d3d72e7e7731962559efc96d3350477e41df0026
Will Howes <williameldenhowes@gmail.com>
2019-10-07 12:34:50 +0100

    refactored tests

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,6 +1,8 @@
 require 'checkout'
 
 describe Checkout do
+
+  describe '#scan' do
     it "returns 50 for one A" do
         expect(subject.scan("A")).to eq(50)
     end
@@ -16,5 +18,6 @@ describe Checkout do
     it 'returns 15 for D' do
       expect(subject.scan("D")).to eq(15)
     end
+  end
 
 end
